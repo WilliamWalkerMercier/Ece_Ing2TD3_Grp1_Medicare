52f737bb9dc5aa16c4ca32b00b056ddfb0c1de1b
Alexandre <131662782+AlexDreams@users.noreply.github.com>
2024-05-29 14:33:59 +0200

    Modifications et ajout
    Modifications et ajout de champs

--- inscription.php
+++ inscription.php
@@ -1,18 +1,22 @@
-<?php 
+<?php
 // Paramètres de connexion à la base de données
 $servername = "localhost";
 $username = "root";
-$dbname = "medicare"; // Nom de la base de données
-$password_mysql = ""; // Mot de passe mysql
+$dbname = "medicare";
+$password_mysql = "mysql";
 
 // Déclaration des variables 
 $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : ""; 
 $nom = isset($_POST["nom"]) ? $_POST["nom"] : ""; 
-$adresse = isset($_POST["adresse"]) ? $_POST["adresse"] : ""; 
+$adresse1 = isset($_POST["adresse1"]) ? $_POST["adresse1"] : ""; 
+$adresse2 = isset($_POST["adresse2"]) ? $_POST["adresse2"] : ""; 
+$ville = isset($_POST["ville"]) ? $_POST["ville"] : ""; 
+$code_postal = isset($_POST["codePostal"]) ? $_POST["codePostal"] : ""; 
+$pays = isset($_POST["pays"]) ? $_POST["pays"] : ""; 
 $email = isset($_POST["email"]) ? $_POST["email"] : ""; 
 $password = isset($_POST["password"]) ? $_POST["password"] : ""; 
 $telephone = isset($_POST["telephone"]) ? $_POST["telephone"] : ""; 
-$carte_vitale = isset($_POST["carte-vitale"]) ? $_POST["carte-vitale"] : ""; 
+$carte_vitale = isset($_POST["carteVitale"]) ? $_POST["carteVitale"] : ""; 
 $erreur = ""; 
 
 // Vérification des champs
@@ -22,8 +26,17 @@ if ($prenom == "") {
 if ($nom == "") { 
     $erreur .= "Le champ Nom est vide. <br>"; 
 } 
-if ($adresse == "") { 
-    $erreur .= "Le champ Adresse est vide. <br>"; 
+if ($adresse1 == "") { 
+    $erreur .= "Le champ Adresse ligne 1 est vide. <br>"; 
+} 
+if ($ville == "") { 
+    $erreur .= "Le champ Ville est vide. <br>"; 
+} 
+if ($code_postal == "") { 
+    $erreur .= "Le champ Code Postal est vide. <br>"; 
+} 
+if ($pays == "") { 
+    $erreur .= "Le champ Pays est vide. <br>"; 
 } 
 if ($email == "") { 
     $erreur .= "Le champ Email est vide. <br>"; 
@@ -55,7 +68,6 @@ if ($conn->connect_error) {
 
 // Préparer et exécuter la requête pour vérifier si l'utilisateur existe déjà
 $sql = "SELECT * FROM Utilisateur WHERE Mail='$email'";
-
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -65,22 +77,18 @@ if ($result->num_rows > 0) {
     // Hacher le mot de passe
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+    // Convertir le numéro de téléphone en entier
+    $telephone = preg_replace('/\D/', '', $telephone);
+    if (substr($telephone, 0, 1) === '0') {
+        $telephone = '33' . substr($telephone, 1);
+    }
+
     // Insérer l'utilisateur dans la table Utilisateur
-    $sql = "INSERT INTO Utilisateur (Nom, Prenom, Mail, Telephone, Mdp, Type) 
-            VALUES ('$nom', '$prenom', '$email', '$telephone', '$hashed_password', 2)";
+    $sql = "INSERT INTO Utilisateur (Nom, Prenom, Mail, Telephone, Mdp, Type, Pays, Ville, Code_Postal, Adresse1, Adresse2, Carte_Vitale) 
+            VALUES ('$nom', '$prenom', '$email', $telephone, '$hashed_password', 2, '$pays', '$ville', '$code_postal', '$adresse1', '$adresse2', '$carte_vitale')";
     
     if ($conn->query($sql) === TRUE) {
-        $last_id = $conn->insert_id;
-        
-        // Insérer les informations du client dans la table Client
-        $sql = "INSERT INTO Client (Id_Client, Type_Carte, Num_Cb, Date_Expiration, Code_Securite) 
-                VALUES ($last_id, 'Vitale', '$carte_vitale', NULL, NULL)";
-        
-        if ($conn->query($sql) === TRUE) {
-            echo "Inscription réussie.";
-        } else {
-            echo "Erreur: " . $sql . "<br>" . $conn->error;
-        }
+        echo "Inscription réussie.";
     } else {
         echo "Erreur: " . $sql . "<br>" . $conn->error;
     }
